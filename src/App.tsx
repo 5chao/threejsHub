cce1f9ff0f8547b7ed63f777f0973444cd759f6c
李武朝 <liwc@udiannet.com>
2022-06-24 17:37:01 +0800

    perf:修改

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Three.js</h1>
+        <h1>Three.js Hub</h1>
       </header>
     </div>
   );
